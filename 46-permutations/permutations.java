dd80316488466fc04f2d502dc8e35eab9b1228c3
Sarthak Seth <sarthakseth80@gmail.com>
2026-07-01 00:07:11 +0530

    Time: 1 ms (87.30%) | Memory: 45.8 MB (16.40%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    private void solve(int[] nums , List<Integer> ds , List<List<Integer>> ans ,boolean freq[]){
+        if(ds.size() == nums.length){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i = 0 ; i < nums.length ; i ++){
+            if(!freq[i]){
+                freq[i] = true;
+                ds.add(nums[i]);
+                solve(nums,ds,ans,freq);
+                ds.remove(ds.size() - 1);
+                freq[i]= false;
+            }
+        }
+
+    }
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+
+        boolean freq[] = new boolean[nums.length];
+
+        solve(nums,ds,ans,freq);
+    
+        return ans;
+    }
+}
+
